ed7b23abbcc62e50f07b4131e5a3f0dc7217dd43
Hassane <hassane.barry@outlook.com>
2021-03-08 23:46:57 +0100

    Initial commit

---
+++ src/main/java/com/student/student/student/StudentController.java
@@ -0,0 +1,45 @@
+package com.student.student.student;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/v1/student")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void registerStudent(@RequestBody Student student){
+        studentService.registerStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void removeStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, @RequestParam(required = false) String name, @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
+    }
+
+    @GetMapping(path = "{studentId}")
+    public Optional<Student> getStudent(@PathVariable("studentId") Long studentId){
+        return studentService.getStudent(studentId);
+    }
+}
---
+++ src/main/java/com/student/student/student/StudentService.java
@@ -0,0 +1,64 @@
+package com.student.student.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void registerStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("email already taken");
+        }
+
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists){
+            throw new IllegalStateException("the student with the ID " + studentId + "does not exist");
+        }
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()-> new IllegalStateException("Student with the ID" + studentId + " does not exists"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            student.setEmail(email);
+        }
+    }
+
+    public Optional<Student> getStudent(Long studentId) {
+        boolean studentExists = studentRepository.existsById(studentId);
+        if (!studentExists){
+            throw new IllegalStateException("Student with id " + studentId + " does not exists");
+        }
+        return studentRepository.findById(studentId);
+    }
+}
